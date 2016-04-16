68c0760891182c4b240b0cc8a431c9b68f7dd9d9
poxstone <poxstone@gmail.com>
2016-04-16 01:37:11 +0000

    complete  crud

--- src/application/static/js/angular/app2.js
+++ src/application/static/js/angular/app2.js
@@ -7,12 +7,12 @@ app.config(configNotification);
 app.config(configRoute);
 app.service('apiRest', ['$http', '$log', 'Notification', ApiRest]);
 app.factory('changeRoute', ['$rootScope', '$route', '$log', ChangeRoute]);
-app.controller('mainController', ['$scope', '$route', '$routeParams', '$location',
+app.controller('mainController', ['$scope', '$rootScope', '$route', '$routeParams', '$location',
   '$http', '$log', 'changeRoute', 'Notification', 'apiRest', 'eforModal', MainController]);
 app.controller('list_controller', ['$scope', ListController]);
 app.controller('ctl_b', ['$scope', CtlB]);
 
-function MainController($scope, $route, $routeParams, $location, $html,
+function MainController($scope, $rootScope, $route, $routeParams, $location, $html,
   $log, changeRoute, notification, apiRest, $eforModal) {
   var mn = this;
   mn.lists;
@@ -44,24 +44,13 @@ function MainController($scope, $route, $routeParams, $location, $html,
 
   mn.openEditItem = function(item) {
     // Prepare item for modal edit
-    mn.item_to_send = angular.copy( item );
-    var callback_close = function() {
-    mn.item_to_send = {};
-    };
-
-    $eforModal.open({templateUrl:'/static/html/list_add2.html', type:'success',
-      scope: mn, onClose:callback_close });
+    mn.item_to_send = angular.copy(item);
+    //$rotScope.templateUrl='/static/html/list_add2.html';
 
   }
 
   mn.openSaveItem = function() {
-    var callback_close = function() {
-      mn.item_to_send = {};
-    };
-
-    $eforModal.open({templateUrl:'/static/html/list_add2.html', type:'success',
-      scope: mn, onClose:callback_close });
-
+    mn.item_to_send = '';
   }
 
   mn.saveItem = function() {
@@ -81,34 +70,25 @@ function MainController($scope, $route, $routeParams, $location, $html,
     $eforModal.close();
     notification('Guardando...');
 
-  }
+  };
 
-  mn.deleteIten = function(item) {
-    $eforModal.confirm({
-      title: 'Delete item',
-      subtitle: item.example_name,
-      confirm_text: 'Delete',
-      cancel_text: 'Cancel',
-      confirmAction: function() {
+  mn.openDeleteItem = function(item) {
+    mn.item_to_send = angular.copy(item);
+  };
 
-        var callbacks = {
-          success : function(res) {
-            notification('Delete item: ' + item.example_name);
-            loadList();
-          },
-          failed : function(res) {
-            $log.debug('error to delete: ' + item.example_name);
-          }
-        };
-
-        // Item_to_send = into modal form
-        apiRest.deleteList( item, callbacks );
-
+  mn.deleteItem = function(item) {
+    var callbacks = {
+      success : function(res) {
+        notification('Delete item: ' + item.example_name);
+        loadList();
       },
-      cancelAction: function() {
-        $log.debug('Cancel delete');
+      failed : function(res) {
+        $log.debug('error to delete: ' + item.example_name);
       }
-    });
+    };
+    // Item_to_send = into modal form
+    apiRest.deleteList( mn.item_to_send, callbacks );
+    delete mn.item_to_send;
 
   }
 
