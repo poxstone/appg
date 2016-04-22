94182b75a3643f3b8fc89a41ac12187e675e9a5e
poxstone <poxstone@gmail.com>
2016-04-22 02:40:11 +0000

    add gulp to work

--- glp/gulpfile.js
+++ glp/gulpfile.js
@@ -1,7 +1,13 @@
 // Dependencias
-var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
 
 // Configuración de la tarea 'demo'
 gulp.task('demo', function () {
@@ -11,4 +17,50 @@ gulp.task('demo', function () {
   .pipe(gulp.dest('js/build/'))
 });
 
-gulp.watch('js/source/*.js', ['demo']);
+// gulp.watch('js/source/*.js', ['demo']);
+
+// css
+gulp.task('css', function () {
+  gulp.src('../src/application/static/css/style.styl')
+    .pipe(stylus({compress: false, paths: ['source/stylus']}))
+    .pipe(stylus({use: [nib()]}))
+    //.pipe(autoprefixer())
+    //.pipe(minifyCSS())
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('../src/application/static/css'))
+});
+
+// html
+gulp.task('html', function() {
+  gulp.src('../src/application/templates/jade/*.jade')
+  .pipe(jade())
+  .pipe(gulp.dest('../src/application/templates'))
+});
+
+gulp.task('html2', function() {
+  gulp.src('../src/application/static/html/jade/*.jade')
+  .pipe(jade())
+  .pipe(gulp.dest('../src/application/static/html'))
+});
+
+// js
+gulp.task('js', function() {
+  gulp.src([
+  'bower_components/jquery/dist/jquery.js',
+  'bower_components/modernizr/modernizr.js'
+  ])
+  .pipe( concat('output.min.js') ) // concat pulls all our files together before minifying them
+  .pipe(uglify())
+  .pipe(gulp.dest('build'))
+});
+
+// watch
+gulp.task('watch', function () {
+   gulp.watch('../src/application/static/css/stylus/*.styl', ['css']);
+   gulp.watch('../src/application/static/css/*.styl', ['css']);
+   gulp.watch('../src/application/static/components/**/*.styl', ['css']);
+   gulp.watch('../src/application/templates/jade/*.jade', ['html']);
+   gulp.watch('../src/application/static/html/jade/*.jade', ['html2']);
+});
+
+gulp.task('default', ['css', 'html', 'html2']);
