72a4eba754c59a8cbe92c538949c8b56cc3a7212
poxstone <poxstone@gmail.com>
2016-06-02 11:46:56 +0000

    add endpoints to run.py

--- run.py
+++ run.py
@@ -4,3 +4,7 @@ import sys
 sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
 sys.path.insert(1, os.path.join(os.path.abspath('.'), 'application'))
 import application
+
+import endpoints
+from apis import GreetingApi, AuthedGreetingApi
+api = endpoints.api_server([GreetingApi, AuthedGreetingApi])
